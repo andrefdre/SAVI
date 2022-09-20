c1d626f723a5e503e98b9e49640d1a8fd01084f4
andref@ua.pt <andref@ua.pt>
2022-09-20 10:58:32 +0100

    hellow world

---
+++ Class_1/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# U   G    O
+
+# U - User , G - Group ,  O - Others
+
+# they have 3 positions 
+#U has r,w,x which corresponds to read write and execute 
+#by default x is 0 for security reasons
+
+#chmod allows to change permissions
+#chmod ugo+rwx
+
+#Do exercises until tutorial 4
+
+print("Hello World!") 
