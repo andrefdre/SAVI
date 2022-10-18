7e2c31b065f099a3a1d5ac70cd8eb43f44f2c8fa
Andre <andref@ua.pt>
2022-10-18 12:03:52 +0100

    class 5

--- Class_4/EX3.py
+++ Class_4/EX3.py
@@ -4,7 +4,7 @@ import numpy as np
 import cv2
 import csv
 import copy
-from functions import Detection,Tracking
+from functions import Detection,Tracker
 
 def main():
 
@@ -13,14 +13,12 @@ def main():
     data_array= csv.reader(open(dataset_location))
 
     #Detection Model
-    body_detector=cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_fullbody.xml')
+    person_detector=cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_fullbody.xml')
     
-    #Template Matching
-    matching_threshold=0.8
-
-    trackers=[] #Create the trackers memory
-    tracker_counter=0
-    iou_threshold=0.8
+    detection_counter = 0
+    tracker_counter = 0
+    trackers = []
+    iou_threshold = 0.8
 
     ####################################################
     #Generate random color for each person and store it#
@@ -41,14 +39,15 @@ def main():
     ############################
     # Processes Frame by Frame #
     ############################
-
+    frame_counter = 0
     while cap.isOpened():
-        ret, frame = cap.read()
-        img_gui = copy.deepcopy(frame) #For printing to not lose original image
-        frame_height, frame_width = frame.shape[:2]
-        current_Frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
-        img_gui = cv2.resize(img_gui, [frame_width//2, frame_height//2])
-        img_gray=cv2.cvtColor(img_gui,cv2.COLOR_BGR2GRAY)
+         # Step 1: get frame
+        ret, image_rgb = cap.read() # get a frame, ret will be true or false if getting succeeds
+        image_gray = cv2.cvtColor(image_rgb, cv2.COLOR_BGR2GRAY)
+        image_gui = copy.deepcopy(image_rgb)
+        if ret == False:
+            break
+        stamp = float(cap.get(cv2.CAP_PROP_POS_MSEC))/1000
 
 
         ##########################################
@@ -74,58 +73,51 @@ def main():
 
         #     cv2.rectangle(img_gui, (bodyLeft,bodyTop), (bodyRight,bodyBottom), (int(color[0]),int(color[1]),int(color[2])), 2, 1)
 
-
-        #################################################
-        # Uses Harr feature of OpenCV to detect people  #
-        #################################################
-
-        #Detect the people in the image
-        bboxes = body_detector.detectMultiScale(img_gray, scaleFactor=1.2,minNeighbors=4,minSize=(30, 30))
-        #bbox is like [x,y,w,h]
+        # ------------------------------------------
+        # Detection of persons 
+        # ------------------------------------------
+        bboxes = person_detector.detectMultiScale(image_gray, scaleFactor=1.2, minNeighbors=4, minSize=(20,40))
 
         detections=[] 
         detection_id = 0 #Id of the detection
         for bbox in bboxes:
-            x,y,w,h = bbox
-            detection = Detection(x,y,w,h,img_gui,detection_id)
+            x1, y1, w, h = bbox
+            detection = Detection(x1, y1, w, h, image_gray, id=detection_counter)
+            detection_counter += 1
+            detection.draw(image_gui)
             detections.append(detection)
-            detection.draw_rectangle(color=[255,0,0])
-            detection_id+=1
+            #cv2.imshow('detection ' + str(detection.id), detection.image  )
             
-
-        #############################################
-        # Create Initial Trackers in first Pixel    #
-        #############################################
-        if current_Frame == 1:
-            for detection in detections:
-                tracker = Tracking(detection, id=tracker_counter)
-                color=colors[tracker_counter,:]
-                detection.draw_rectangle(color)
-                trackers.append(tracker)
-                tracker_counter += 1
 
 
         #######################################################
         # Compares the new detections to the old ones(tracking)
         #######################################################
-        for detection in detections:
-            for tracker in trackers:
-                iou = detection.computeIOU(tracker.detections[-1])
-                #print('IOU( T' + str(tracker.id) + ' D' + str(detection.id) + ' ) = ' + str(iou))
-                res = cv2.matchTemplate(img_gui,detection.image,eval('cv2.TM_CCOEFF_NORMED'))
-                print(np.any(res>=matching_threshold))
-                print(res)
-                if iou > iou_threshold or np.any(res>=matching_threshold):
+        for detection in detections: # cycle all detections
+            for tracker in trackers: # cycle all trackers
+                tracker_bbox = tracker.detections[-1]
+                iou = detection.computeIOU(tracker_bbox)
+                print('IOU( T' + str(tracker.id) + ' D' + str(detection.id) + ' ) = ' + str(iou))
+                if iou > iou_threshold: # associate detection with tracker 
                     tracker.addDetection(detection)
-                    print(detections.index(detection))
-                    ##detections.pop(detections.index(detection))
-                    color=colors[tracker_counter,:]
-                    tracker.draw_rectangle(img_gui,color)
 
-        ##################################################
-        #Removed used detections
-        ##################################################
-            
+
+         # ------------------------------------------
+        # Create Tracker for each detection
+        # ------------------------------------------
+            for detection in detections:
+                if not detection.tracker_assigned:
+                    tracker = Tracker(detection, id=tracker_counter)
+                    tracker_counter += 1
+                    trackers.append(tracker)
+
+        # Draw trackers
+        for tracker in trackers:
+            tracker.draw(image_gui)
+        
+        print(trackers)
+
+        frame_counter += 1    
 
 
 
@@ -143,7 +135,7 @@ def main():
         #     tracker_counter += 1
 
 
-        cv2.imshow('Initial', img_gui)
+        cv2.imshow("window_name",image_gui) # show the image
         if cv2.waitKey(0) == ord('q'):
             break
 
--- Class_4/functions.py
+++ Class_4/functions.py
@@ -32,37 +32,65 @@ class Bounding_box():
         
         return A_intr / float(A_union)
 
-class Detection(Bounding_box):
-    def __init__(self, x, y, w, h,img_gui,id):
-        super().__init__(x, y, w, h)
-        self.img_gui=img_gui
-        self.id=id
-        self.extractSmallImage(img_gui)
-
     def extractSmallImage(self, image_full):
-        self.image = image_full[self.y1:self.y1+self.h, self.x1:self.x1+self.w]
-
-    def draw_rectangle(self,color):
-        cv2.rectangle(self.img_gui, (self.x1,self.y1), (self.x2,self.y2), (int(color[0]),int(color[1]),int(color[2])), 2, 1)
-        cv2.putText(self.img_gui, 'DET ' + str(self.id), (self.x1,self.y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (int(color[0]),int(color[1]),int(color[2])), 1, cv2.LINE_AA)
+        return  image_full[self.y1:self.y1+self.h, self.x1:self.x1+self.w]
 
 
-class Tracking():
+class Detection(Bounding_box):
+    def __init__(self, x, y, w, h,img_full,id):
+        super().__init__(x, y, w, h)
+        self.id=id
+        self.image = self.extractSmallImage(img_full)
+        self.tracker_assigned=False
+
+
+    def draw(self, image_gui, color=(255,0,0)):
+        cv2.rectangle(image_gui,(self.x1,self.y1),(self.x2, self.y2),color,3)
+
+        image = cv2.putText(image_gui, 'D' + str(self.id), (self.x1, self.y1-5), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv2.LINE_AA)
+
+class Tracker():
     def __init__(self,detection,id):
         self.detections=[detection]
         self.id=id
+        self.Template=None
+        self.bboxes = []
 
-    def draw_rectangle(self,img_gui,color):
+    def draw_last_detection(self,img_gui,color=(255,0,255)):
         last_detection = self.detections[-1] # get the last detection
         cv2.rectangle(img_gui, (last_detection.x1,last_detection.y1), (last_detection.x2,last_detection.y2), (int(color[0]),int(color[1]),int(color[2])), 2, 1)
-        cv2.putText(img_gui, 'TRK ' + str(self.id), (last_detection.x1+50,last_detection.y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (int(color[0]),int(color[1]),int(color[2])), 1, cv2.LINE_AA)
+        cv2.putText(img_gui, 'TRK ' + str(self.id), (last_detection.x1+50,last_detection.y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1, cv2.LINE_AA)
    
+    def draw(self,img_gui,color=(255,0,255)):
+        bbox= self.bboxes[-1]
+        cv2.rectangle(img_gui, (bbox.x1,bbox.y1), (bbox.x1+bbox.w,bbox.y2+bbox.h), (int(color[0]),int(color[1]),int(color[2])), 2, 1)
+        cv2.putText(img_gui, 'TRK ' + str(self.id), (bbox.x1+50,bbox.y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1, cv2.LINE_AA)
+   
+    
     def addDetection(self, detection):
         self.detections.append(detection)
+        detection.tracker_assigned=True
+        self.Template=detection.image
+        bbox= Bounding_box(detection.x1,detection.y1,detection.h,detection.w)
+        self.bboxes.append(bbox)
+
+    def track(self,image):
+        h,w=self.Template.shape
+        image_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        result = cv2.matchTemplate(image_gray,self.Template,cv2.TM_CCOEFF_NORMED)
+        _,max_val , _ , max_loc = cv2.minMaxLoc(result)
+
+        x1=max_loc[0]
+        y1=max_loc[1]
+
+        self.Template=bbox.extractSmallImage(image)
+
+        bbox= Bounding_box(x1,y1,h,w)
+        self.bboxes.append(bbox)
 
     def __str__(self):
         text =  'T' + str(self.id) + ' Detections = ['
         for detection in self.detections:
             text += str(detection.id) + ', '
 
-        return 
+        return text
